fcabafd78dc25f6346b3d187fba56c6705ce0e20
tobiasuihlein <tobias.uihlein@gmail.com>
2024-11-20 16:03:11 +0100

    move some settings to .env

--- music_game_app/music_game_app/settings.py
+++ music_game_app/music_game_app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4tiv@k8*%ei!$=owvyp^3$a8%gz*t1z8tq)f0ggpw&van_l27#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED', 'localhost').split(',')
 
 
 # Application definition
