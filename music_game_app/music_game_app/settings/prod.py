3646cea0d6c64b90040a6ac2e1ebab5c48111e80
tobiasuihlein <tobias.uihlein@gmail.com>
2024-11-27 15:07:17 +0100

    adjusted csrf settings for production

--- music_game_app/music_game_app/settings/prod.py
+++ music_game_app/music_game_app/settings/prod.py
@@ -1,6 +1,7 @@
 from .base import *
 
 DEBUG = True
+
 ALLOWED_HOSTS = [
     'tobiasuihlein.de',
     'www.tobiasuihlein.de',
@@ -11,13 +12,21 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://tobiasuihlein.de',
+    'https://www.tobiasuihlein.de'
+]
+
 # Trust X-Forwarted-Proto header from reverse proxy after SSL termination
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# SSL termination is done by the proxy
+SECURE_SSL_REDIRECT = False
 
 # Session settings
 SESSION_COOKIE_AGE = 86400  # 24 hours
 
+# Static files settings
 STATIC_ROOT = '/app/staticfiles'
 STATIC_URL = 'static/'
-
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
